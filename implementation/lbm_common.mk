5a626f9747b17dbd8f7a141f1e2dd3a0bdcb0ae5
Dwii <adrien.python@gmail.com>
2017-06-01 21:43:41 +0200

    Replace makefile timing function by execute which handle (DY)LD_LIBRARY_PROFILE export

--- implementation/lbm_common.mk
+++ implementation/lbm_common.mk
@@ -1,13 +1,22 @@
-# Function:    timing
-# Description: Store the execution time and LUPS of a LBM simulation in a file
+noop=
+space=$(noop) $(noop)
+
+# Function:    execute
+# Description: Export list of libraries path, execute command and store the 
+#              execution time and LUPS of a LBM simulation if a timing file 
+#              path is provided.
 # Argument $1: LBM simulation command (in a string) to execute
 # Argument $2: File where the timing and lups are stored
-define timing
-	@start=$$(date +%s); \
-	echo $$(eval "$(1)" | sed -n 's/average lups: \(.*\).*/\1/p') > $(2); \
+# Argument $3: (optional) timing file path
+define execute
+	@timing=$$(echo "$(3)" | xargs); \
+	[[ "$$timing" == "" ]] && timing=/dev/null; \
+	export DYLD_LIBRARY_PATH=$$DYLD_LIBRARY_PATH$(subst $(space):,:,$(addprefix :,$(1))); \
+	start=$$(date +%s); \
+	echo $$(eval $(2)) | sed -n 's/.*average lups: \(.*\).*/\1/p' > $$timing; \
 	end=$$(date +%s); \
 	T=$$(($$end-$$start)); \
-	printf "$$T" >> $(2);
+	printf "$$T" >> $$timing;
 endef
 
 # Binary arguments
