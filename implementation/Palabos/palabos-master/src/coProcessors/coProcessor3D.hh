b3ab9dfb40cbbb12d4b19c91b3404464665caac0
Dwii <adrien.python@gmail.com>
2017-06-29 00:25:20 +0200

    Fix bug and cleanup

--- implementation/Palabos/palabos-master/src/coProcessors/coProcessor3D.hh
+++ implementation/Palabos/palabos-master/src/coProcessors/coProcessor3D.hh
@@ -102,7 +102,6 @@ namespace plb {
     int D3Q19CudaCoProcessor3D<T>::addDomain(plint nx, plint ny, plint nz, T omega, int& domainHandle)
     //int D3Q19CudaCoProcessor3D<T>::addDomain(Box3D const& domain, T omega)
     {
-        printf("addDomain\n");
         this->nx = nx;
         this->ny = ny;
         this->nz = nz;
@@ -126,42 +125,39 @@ namespace plb {
     template<typename T>
     int D3Q19CudaCoProcessor3D<T>::send(int domainHandle, Box3D const& subDomain, std::vector<char> const& data)
     {
-//        printf("send %d\n", domainHandle);
         T const* pal_lattices = (const double *)&data[0];
 
-//        if ( ! lattices_on_host ) 
+        if ( ! lattices_on_host ) 
         {
             lbm_lattices_read(lbm_sim, lattices);
-//            lattices_on_host = true;
+            lattices_on_host = true;
         }
+
 
         for (plint x = subDomain.x0; x <= subDomain.x1; x++) {
             for (plint y = subDomain.y0; y <= subDomain.y1; y++) {
                 for (plint z = subDomain.z0; z <= subDomain.z1; z++) {
                     size_t gi = IDX(x,y,z,nx,ny,nz);
 
-                    lattices->c [gi] = pal_lattices[PIDX(x,y,z, 0,nx,ny,nz)];
-                    lattices->w [gi] = pal_lattices[PIDX(x,y,z, 1,nx,ny,nz)];
-                    lattices->s [gi] = pal_lattices[PIDX(x,y,z, 2,nx,ny,nz)];
-                    lattices->bc[gi] = pal_lattices[PIDX(x,y,z, 3,nx,ny,nz)];
-                    lattices->sw[gi] = pal_lattices[PIDX(x,y,z, 4,nx,ny,nz)];
-                    lattices->nw[gi] = pal_lattices[PIDX(x,y,z, 5,nx,ny,nz)];
-                    lattices->bw[gi] = pal_lattices[PIDX(x,y,z, 6,nx,ny,nz)];
-                    lattices->tw[gi] = pal_lattices[PIDX(x,y,z, 7,nx,ny,nz)];
-                    lattices->bs[gi] = pal_lattices[PIDX(x,y,z, 8,nx,ny,nz)];
-                    lattices->ts[gi] = pal_lattices[PIDX(x,y,z, 9,nx,ny,nz)];
-                    lattices->e [gi] = pal_lattices[PIDX(x,y,z,10,nx,ny,nz)];
-                    lattices->n [gi] = pal_lattices[PIDX(x,y,z,11,nx,ny,nz)];
-                    lattices->tc[gi] = pal_lattices[PIDX(x,y,z,12,nx,ny,nz)];
-                    lattices->ne[gi] = pal_lattices[PIDX(x,y,z,13,nx,ny,nz)];
-                    lattices->se[gi] = pal_lattices[PIDX(x,y,z,14,nx,ny,nz)];
-                    lattices->te[gi] = pal_lattices[PIDX(x,y,z,15,nx,ny,nz)];
-                    lattices->be[gi] = pal_lattices[PIDX(x,y,z,16,nx,ny,nz)];
-                    lattices->tn[gi] = pal_lattices[PIDX(x,y,z,17,nx,ny,nz)];
-                    lattices->bn[gi] = pal_lattices[PIDX(x,y,z,18,nx,ny,nz)];
-
-//                   memcpy((void*)(&lattices->c [gi]), (const void*)&(pal_lattices[PIDX(x, y, z,  0, nx,ny,nz)]), sizeof(T));
-//                   memcpy((void*)(&lattices->c [gi]), (const void*)(&data[PIDX(x,y,z, 0*sizeof(T),nx,ny,nz)]), sizeof(T));
+                    lattices->c [gi] = pal_lattices[PIDX(x,y,z, 0,nx,ny,nz)] + 1./3 ;
+                    lattices->w [gi] = pal_lattices[PIDX(x,y,z, 1,nx,ny,nz)] + 1./18;
+                    lattices->s [gi] = pal_lattices[PIDX(x,y,z, 2,nx,ny,nz)] + 1./18;
+                    lattices->bc[gi] = pal_lattices[PIDX(x,y,z, 3,nx,ny,nz)] + 1./18;
+                    lattices->sw[gi] = pal_lattices[PIDX(x,y,z, 4,nx,ny,nz)] + 1./36;
+                    lattices->nw[gi] = pal_lattices[PIDX(x,y,z, 5,nx,ny,nz)] + 1./36;
+                    lattices->bw[gi] = pal_lattices[PIDX(x,y,z, 6,nx,ny,nz)] + 1./36;
+                    lattices->tw[gi] = pal_lattices[PIDX(x,y,z, 7,nx,ny,nz)] + 1./36;
+                    lattices->bs[gi] = pal_lattices[PIDX(x,y,z, 8,nx,ny,nz)] + 1./36;
+                    lattices->ts[gi] = pal_lattices[PIDX(x,y,z, 9,nx,ny,nz)] + 1./36;
+                    lattices->e [gi] = pal_lattices[PIDX(x,y,z,10,nx,ny,nz)] + 1./18;
+                    lattices->n [gi] = pal_lattices[PIDX(x,y,z,11,nx,ny,nz)] + 1./18;
+                    lattices->tc[gi] = pal_lattices[PIDX(x,y,z,12,nx,ny,nz)] + 1./18;
+                    lattices->ne[gi] = pal_lattices[PIDX(x,y,z,13,nx,ny,nz)] + 1./36;
+                    lattices->se[gi] = pal_lattices[PIDX(x,y,z,14,nx,ny,nz)] + 1./36;
+                    lattices->te[gi] = pal_lattices[PIDX(x,y,z,15,nx,ny,nz)] + 1./36;
+                    lattices->be[gi] = pal_lattices[PIDX(x,y,z,16,nx,ny,nz)] + 1./36;
+                    lattices->tn[gi] = pal_lattices[PIDX(x,y,z,17,nx,ny,nz)] + 1./36;
+                    lattices->bn[gi] = pal_lattices[PIDX(x,y,z,18,nx,ny,nz)] + 1./36;
                 }
             }
         }
@@ -173,15 +169,15 @@ namespace plb {
     template<typename T>
     int D3Q19CudaCoProcessor3D<T>::receive(int domainHandle, Box3D const& subDomain, std::vector<char>& data) const
     {
-//        printf("receive %d\n", domainHandle);
         data.resize(nx*ny*nz*19*sizeof(T));
 
         T* pal_lattices = (double *)&data[0];
 
-//        if ( ! lattices_on_host ) 
+        if ( ! lattices_on_host ) 
         {
             lbm_lattices_read(lbm_sim, lattices);
-//            lattices_on_host = true;
+            bool answer = true;
+            memcpy((void*)&this->lattices_on_host, (const void*)&answer, sizeof(bool));
         }
 
         for (plint x = subDomain.x0; x <= subDomain.x1; x++) {
@@ -189,29 +185,25 @@ namespace plb {
                 for (plint z = subDomain.z0; z <= subDomain.z1; z++) {
                     size_t gi = IDX(x,y,z,nx,ny,nz);
                     
-                    pal_lattices[PIDX(x,y,z, 0,nx,ny,nz)] = lattices->c [gi];
-                    pal_lattices[PIDX(x,y,z, 1,nx,ny,nz)] = lattices->w [gi];
-                    pal_lattices[PIDX(x,y,z, 2,nx,ny,nz)] = lattices->s [gi];
-                    pal_lattices[PIDX(x,y,z, 3,nx,ny,nz)] = lattices->bc[gi];
-                    pal_lattices[PIDX(x,y,z, 4,nx,ny,nz)] = lattices->sw[gi];
-                    pal_lattices[PIDX(x,y,z, 5,nx,ny,nz)] = lattices->nw[gi];
-                    pal_lattices[PIDX(x,y,z, 6,nx,ny,nz)] = lattices->bw[gi];
-                    pal_lattices[PIDX(x,y,z, 7,nx,ny,nz)] = lattices->tw[gi];
-                    pal_lattices[PIDX(x,y,z, 8,nx,ny,nz)] = lattices->bs[gi];
-                    pal_lattices[PIDX(x,y,z, 9,nx,ny,nz)] = lattices->ts[gi];
-                    pal_lattices[PIDX(x,y,z,10,nx,ny,nz)] = lattices->e [gi];
-                    pal_lattices[PIDX(x,y,z,11,nx,ny,nz)] = lattices->n [gi];
-                    pal_lattices[PIDX(x,y,z,12,nx,ny,nz)] = lattices->tc[gi];
-                    pal_lattices[PIDX(x,y,z,13,nx,ny,nz)] = lattices->ne[gi];
-                    pal_lattices[PIDX(x,y,z,14,nx,ny,nz)] = lattices->se[gi];
-                    pal_lattices[PIDX(x,y,z,15,nx,ny,nz)] = lattices->te[gi];
-                    pal_lattices[PIDX(x,y,z,16,nx,ny,nz)] = lattices->be[gi];
-                    pal_lattices[PIDX(x,y,z,17,nx,ny,nz)] = lattices->tn[gi];
-                    pal_lattices[PIDX(x,y,z,18,nx,ny,nz)] = lattices->bn[gi];
-
-//                    memcpy((void*)(&data[PIDX(x,y,z, 0*sizeof(T),nx,ny,nz)]), (const void*)(&lattices->c [gi]), sizeof(T));
-//                    memcpy((void*)&(pal_lattices[PIDX(x, y, z,  0, nx,ny,nz)]), (const void*)(&lattices->c [gi]), sizeof(T));
-
+                    pal_lattices[PIDX(x,y,z, 0,nx,ny,nz)] = lattices->c [gi] - 1./3 ;
+                    pal_lattices[PIDX(x,y,z, 1,nx,ny,nz)] = lattices->w [gi] - 1./18;
+                    pal_lattices[PIDX(x,y,z, 2,nx,ny,nz)] = lattices->s [gi] - 1./18;
+                    pal_lattices[PIDX(x,y,z, 3,nx,ny,nz)] = lattices->bc[gi] - 1./18;
+                    pal_lattices[PIDX(x,y,z, 4,nx,ny,nz)] = lattices->sw[gi] - 1./36;
+                    pal_lattices[PIDX(x,y,z, 5,nx,ny,nz)] = lattices->nw[gi] - 1./36;
+                    pal_lattices[PIDX(x,y,z, 6,nx,ny,nz)] = lattices->bw[gi] - 1./36;
+                    pal_lattices[PIDX(x,y,z, 7,nx,ny,nz)] = lattices->tw[gi] - 1./36;
+                    pal_lattices[PIDX(x,y,z, 8,nx,ny,nz)] = lattices->bs[gi] - 1./36;
+                    pal_lattices[PIDX(x,y,z, 9,nx,ny,nz)] = lattices->ts[gi] - 1./36;
+                    pal_lattices[PIDX(x,y,z,10,nx,ny,nz)] = lattices->e [gi] - 1./18;
+                    pal_lattices[PIDX(x,y,z,11,nx,ny,nz)] = lattices->n [gi] - 1./18;
+                    pal_lattices[PIDX(x,y,z,12,nx,ny,nz)] = lattices->tc[gi] - 1./18;
+                    pal_lattices[PIDX(x,y,z,13,nx,ny,nz)] = lattices->ne[gi] - 1./36;
+                    pal_lattices[PIDX(x,y,z,14,nx,ny,nz)] = lattices->se[gi] - 1./36;
+                    pal_lattices[PIDX(x,y,z,15,nx,ny,nz)] = lattices->te[gi] - 1./36;
+                    pal_lattices[PIDX(x,y,z,16,nx,ny,nz)] = lattices->be[gi] - 1./36;
+                    pal_lattices[PIDX(x,y,z,17,nx,ny,nz)] = lattices->tn[gi] - 1./36;
+                    pal_lattices[PIDX(x,y,z,18,nx,ny,nz)] = lattices->bn[gi] - 1./36;
                 }
             }
         }
@@ -222,7 +214,6 @@ namespace plb {
     template<typename T>
     int D3Q19CudaCoProcessor3D<T>::collideAndStream(int domainHandle)
     {
-        //printf("collideAndStream\n");
         lbm_simulation_update(lbm_sim);
         lattices_on_host = false;
         return 1;
