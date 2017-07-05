c72c3f3682c329023bbd6c84d0e2f21e6f8fb2a7
Dwii <adrien.python@gmail.com>
2017-07-06 00:11:27 +0200

    Allow makefile to run either on Mac OSX or Linux
    - Set bash as default shell (mainly for the [[ ]] test which do not run
    with sh)
    - Set the right variable (LD_LIBRARY_PATH or DYLD_LIBRARY_PATH)
    depending on the OS

--- implementation/lbm_common.mk
+++ implementation/lbm_common.mk
@@ -1,5 +1,6 @@
 noop=
 space=$(noop) $(noop)
+SHELL=/bin/bash
 
 # Function:    execute
 # Description: Export list of libraries path, execute command and store the 
@@ -10,8 +11,9 @@ space=$(noop) $(noop)
 # Argument $3: (optional) timing file path
 define execute
 	@timing=$$(echo "$(3)" | xargs); \
+	llp_var=$(if $(filter $(shell uname),Darwin),"DYLD_LIBRARY_PATH","LD_LIBRARY_PATH"); \
 	[[ "$$timing" == "" ]] && timing=/dev/null; \
-	export DYLD_LIBRARY_PATH=$$DYLD_LIBRARY_PATH$(subst $(space):,:,$(addprefix :,$(1))); \
+	eval "export $$llp_var=$$$$llp_var$(subst $(space):,:,$(addprefix :,$(1)))"; \
 	start=$$(date +%s); \
 	echo $$(eval "$(2) | tee $(STD_FILE)" ) | sed -n 's/.*average lups: \(.*\).*/\1/p' > $$timing; \
 	end=$$(date +%s); \
