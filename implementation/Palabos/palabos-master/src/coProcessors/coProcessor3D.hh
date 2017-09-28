e75e5c94d27faf21a1746a960142e4af074d8af5
Dwii <adrien.python@gmail.com>
2017-09-28 16:39:27 +0200

    Fix send and receive methods to work when subdomain < domain

--- implementation/Palabos/palabos-master/src/coProcessors/coProcessor3D.hh
+++ implementation/Palabos/palabos-master/src/coProcessors/coProcessor3D.hh
@@ -81,7 +81,7 @@ namespace plb {
     
     /**** Cuda coProcessor ****/
 
-    #define PIDX(x, y, z, f, nx, ny, nz) ( ( x*(nz*ny) + y*(nz) + z) * 19 + f)
+    #define PBIDX(x, y, z, nx, ny, nz) ( ( x*(nz*ny) + y*(nz) + z) * 19)
 
     template<typename T>
     D3Q19CudaCoProcessor3D<T>::D3Q19CudaCoProcessor3D() 
@@ -135,31 +135,39 @@ namespace plb {
             lattices_on_host = true;
         }
 
+        long snx = std::abs(subDomain.x0 - subDomain.x1) + 1;
+        long sny = std::abs(subDomain.y0 - subDomain.y1) + 1;
+        long snz = std::abs(subDomain.z0 - subDomain.z1) + 1;
 
         for (plint x = subDomain.x0; x <= subDomain.x1; x++) {
             for (plint y = subDomain.y0; y <= subDomain.y1; y++) {
                 for (plint z = subDomain.z0; z <= subDomain.z1; z++) {
                     size_t gi = IDX(x,y,z,nx,ny,nz);
 
-                    lattices->c [gi] = pal_lattices[PIDX(x,y,z, 0,nx,ny,nz)] + 1./3 ;
-                    lattices->w [gi] = pal_lattices[PIDX(x,y,z, 1,nx,ny,nz)] + 1./18;
-                    lattices->s [gi] = pal_lattices[PIDX(x,y,z, 2,nx,ny,nz)] + 1./18;
-                    lattices->bc[gi] = pal_lattices[PIDX(x,y,z, 3,nx,ny,nz)] + 1./18;
-                    lattices->sw[gi] = pal_lattices[PIDX(x,y,z, 4,nx,ny,nz)] + 1./36;
-                    lattices->nw[gi] = pal_lattices[PIDX(x,y,z, 5,nx,ny,nz)] + 1./36;
-                    lattices->bw[gi] = pal_lattices[PIDX(x,y,z, 6,nx,ny,nz)] + 1./36;
-                    lattices->tw[gi] = pal_lattices[PIDX(x,y,z, 7,nx,ny,nz)] + 1./36;
-                    lattices->bs[gi] = pal_lattices[PIDX(x,y,z, 8,nx,ny,nz)] + 1./36;
-                    lattices->ts[gi] = pal_lattices[PIDX(x,y,z, 9,nx,ny,nz)] + 1./36;
-                    lattices->e [gi] = pal_lattices[PIDX(x,y,z,10,nx,ny,nz)] + 1./18;
-                    lattices->n [gi] = pal_lattices[PIDX(x,y,z,11,nx,ny,nz)] + 1./18;
-                    lattices->tc[gi] = pal_lattices[PIDX(x,y,z,12,nx,ny,nz)] + 1./18;
-                    lattices->ne[gi] = pal_lattices[PIDX(x,y,z,13,nx,ny,nz)] + 1./36;
-                    lattices->se[gi] = pal_lattices[PIDX(x,y,z,14,nx,ny,nz)] + 1./36;
-                    lattices->te[gi] = pal_lattices[PIDX(x,y,z,15,nx,ny,nz)] + 1./36;
-                    lattices->be[gi] = pal_lattices[PIDX(x,y,z,16,nx,ny,nz)] + 1./36;
-                    lattices->tn[gi] = pal_lattices[PIDX(x,y,z,17,nx,ny,nz)] + 1./36;
-                    lattices->bn[gi] = pal_lattices[PIDX(x,y,z,18,nx,ny,nz)] + 1./36;
+                    size_t px = x - subDomain.x0;
+                    size_t py = y - subDomain.y0;
+                    size_t pz = z - subDomain.z0;
+                    size_t pbi = PBIDX(px,py,pz,snx,sny,snz); // base index
+
+                    lattices->c [gi] = pal_lattices[pbi +  0] + 1./3 ;
+                    lattices->w [gi] = pal_lattices[pbi +  1] + 1./18;
+                    lattices->s [gi] = pal_lattices[pbi +  2] + 1./18;
+                    lattices->bc[gi] = pal_lattices[pbi +  3] + 1./18;
+                    lattices->sw[gi] = pal_lattices[pbi +  4] + 1./36;
+                    lattices->nw[gi] = pal_lattices[pbi +  5] + 1./36;
+                    lattices->bw[gi] = pal_lattices[pbi +  6] + 1./36;
+                    lattices->tw[gi] = pal_lattices[pbi +  7] + 1./36;
+                    lattices->bs[gi] = pal_lattices[pbi +  8] + 1./36;
+                    lattices->ts[gi] = pal_lattices[pbi +  9] + 1./36;
+                    lattices->e [gi] = pal_lattices[pbi + 10] + 1./18;
+                    lattices->n [gi] = pal_lattices[pbi + 11] + 1./18;
+                    lattices->tc[gi] = pal_lattices[pbi + 12] + 1./18;
+                    lattices->ne[gi] = pal_lattices[pbi + 13] + 1./36;
+                    lattices->se[gi] = pal_lattices[pbi + 14] + 1./36;
+                    lattices->te[gi] = pal_lattices[pbi + 15] + 1./36;
+                    lattices->be[gi] = pal_lattices[pbi + 16] + 1./36;
+                    lattices->tn[gi] = pal_lattices[pbi + 17] + 1./36;
+                    lattices->bn[gi] = pal_lattices[pbi + 18] + 1./36;
                 }
             }
         }
@@ -171,8 +179,11 @@ namespace plb {
     template<typename T>
     int D3Q19CudaCoProcessor3D<T>::receive(int domainHandle, Box3D const& subDomain, std::vector<char>& data) const
     {
-        data.resize(nx*ny*nz*19*sizeof(T));
-
+        long snx = std::abs(subDomain.x0 - subDomain.x1) + 1;
+        long sny = std::abs(subDomain.y0 - subDomain.y1) + 1;
+        long snz = std::abs(subDomain.z0 - subDomain.z1) + 1;
+        data.resize(snx*sny*snz*19*sizeof(T));
+        
         T* pal_lattices = (double *)&data[0];
 
         if ( ! lattices_on_host ) 
@@ -187,25 +198,30 @@ namespace plb {
                 for (plint z = subDomain.z0; z <= subDomain.z1; z++) {
                     size_t gi = IDX(x,y,z,nx,ny,nz);
                     
-                    pal_lattices[PIDX(x,y,z, 0,nx,ny,nz)] = lattices->c [gi] - 1./3 ;
-                    pal_lattices[PIDX(x,y,z, 1,nx,ny,nz)] = lattices->w [gi] - 1./18;
-                    pal_lattices[PIDX(x,y,z, 2,nx,ny,nz)] = lattices->s [gi] - 1./18;
-                    pal_lattices[PIDX(x,y,z, 3,nx,ny,nz)] = lattices->bc[gi] - 1./18;
-                    pal_lattices[PIDX(x,y,z, 4,nx,ny,nz)] = lattices->sw[gi] - 1./36;
-                    pal_lattices[PIDX(x,y,z, 5,nx,ny,nz)] = lattices->nw[gi] - 1./36;
-                    pal_lattices[PIDX(x,y,z, 6,nx,ny,nz)] = lattices->bw[gi] - 1./36;
-                    pal_lattices[PIDX(x,y,z, 7,nx,ny,nz)] = lattices->tw[gi] - 1./36;
-                    pal_lattices[PIDX(x,y,z, 8,nx,ny,nz)] = lattices->bs[gi] - 1./36;
-                    pal_lattices[PIDX(x,y,z, 9,nx,ny,nz)] = lattices->ts[gi] - 1./36;
-                    pal_lattices[PIDX(x,y,z,10,nx,ny,nz)] = lattices->e [gi] - 1./18;
-                    pal_lattices[PIDX(x,y,z,11,nx,ny,nz)] = lattices->n [gi] - 1./18;
-                    pal_lattices[PIDX(x,y,z,12,nx,ny,nz)] = lattices->tc[gi] - 1./18;
-                    pal_lattices[PIDX(x,y,z,13,nx,ny,nz)] = lattices->ne[gi] - 1./36;
-                    pal_lattices[PIDX(x,y,z,14,nx,ny,nz)] = lattices->se[gi] - 1./36;
-                    pal_lattices[PIDX(x,y,z,15,nx,ny,nz)] = lattices->te[gi] - 1./36;
-                    pal_lattices[PIDX(x,y,z,16,nx,ny,nz)] = lattices->be[gi] - 1./36;
-                    pal_lattices[PIDX(x,y,z,17,nx,ny,nz)] = lattices->tn[gi] - 1./36;
-                    pal_lattices[PIDX(x,y,z,18,nx,ny,nz)] = lattices->bn[gi] - 1./36;
+                    size_t px = x - subDomain.x0;
+                    size_t py = y - subDomain.y0;
+                    size_t pz = z - subDomain.z0;
+                    size_t pbi = PBIDX(px,py,pz,snx,sny,snz); // base index
+
+                    pal_lattices[pbi +  0] = lattices->c [gi] - 1./3 ;
+                    pal_lattices[pbi +  1] = lattices->w [gi] - 1./18;
+                    pal_lattices[pbi +  2] = lattices->s [gi] - 1./18;
+                    pal_lattices[pbi +  3] = lattices->bc[gi] - 1./18;
+                    pal_lattices[pbi +  4] = lattices->sw[gi] - 1./36;
+                    pal_lattices[pbi +  5] = lattices->nw[gi] - 1./36;
+                    pal_lattices[pbi +  6] = lattices->bw[gi] - 1./36;
+                    pal_lattices[pbi +  7] = lattices->tw[gi] - 1./36;
+                    pal_lattices[pbi +  8] = lattices->bs[gi] - 1./36;
+                    pal_lattices[pbi +  9] = lattices->ts[gi] - 1./36;
+                    pal_lattices[pbi + 10] = lattices->e [gi] - 1./18;
+                    pal_lattices[pbi + 11] = lattices->n [gi] - 1./18;
+                    pal_lattices[pbi + 12] = lattices->tc[gi] - 1./18;
+                    pal_lattices[pbi + 13] = lattices->ne[gi] - 1./36;
+                    pal_lattices[pbi + 14] = lattices->se[gi] - 1./36;
+                    pal_lattices[pbi + 15] = lattices->te[gi] - 1./36;
+                    pal_lattices[pbi + 16] = lattices->be[gi] - 1./36;
+                    pal_lattices[pbi + 17] = lattices->tn[gi] - 1./36;
+                    pal_lattices[pbi + 18] = lattices->bn[gi] - 1./36;
                 }
             }
         }
