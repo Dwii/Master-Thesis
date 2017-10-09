5a39df3d6e3a7d313c0dd1f1cf92bfd52debecc8
Dwii <adrien.python@gmail.com>
2017-10-10 00:00:33 +0200

    Refactor execute and check functions and rename "timing" files as "info"

--- implementation/lbm_common.mk
+++ implementation/lbm_common.mk
@@ -4,21 +4,21 @@ SHELL=/bin/bash
 
 # Function:    execute
 # Description: Export list of libraries path, execute command and store the 
-#              execution time and LUPS of a LBM simulation if a timing file 
+#              execution time and LUPS of a LBM simulation if a info file 
 #              path is provided.
 # Argument $1: Dynamic libraries paths (separated by spaces)
 # Argument $2: LBM simulation command (in a string) to execute
-# Argument $3: (optional) timing file path
+# Argument $3: (optional) info file path
 define execute
-	@timing=$$(echo "$(3)" | xargs); \
+	@info=$$(echo "$(3)" | xargs); \
 	llp_var=$(if $(filter $(shell uname),Darwin),"DYLD_LIBRARY_PATH","LD_LIBRARY_PATH"); \
-	[[ "$$timing" == "" ]] && timing=/dev/null; \
+	[[ "$$info" == "" ]] && info=/dev/null; \
 	eval "export $$llp_var=$${!llp_var}$(subst $(space):,:,$(addprefix :,$(1)))"; \
 	start=$$(date +%s); \
-	echo $$(eval "$(2) | tee $(STD_FILE)" ) | sed -n 's/.*average lups: \(.*\).*/\1/p' > $$timing; \
+	echo $$(eval $(2) | tee $(STD_FILE) ) > $$info; \
 	end=$$(date +%s); \
 	T=$$(($$end-$$start)); \
-	printf "$$T" >> $$timing;
+	printf "execution time: $$T" >> $$info;
 endef
 
 # Rule .stdout: set STD_FILE as the standard output for execute
