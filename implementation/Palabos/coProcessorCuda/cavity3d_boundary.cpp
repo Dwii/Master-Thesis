4929ba7a0695e57c871480c5bef13343ad3962e6
Dwii <adrien.python@gmail.com>
2017-10-31 17:06:24 +0100

    Print which block is run by which processor

--- implementation/Palabos/coProcessorCuda/cavity3d_boundary.cpp
+++ implementation/Palabos/coProcessorCuda/cavity3d_boundary.cpp
@@ -197,6 +197,7 @@ int main(int argc, char* argv[]) {
     for (pluint iProc=0; iProc<ranges.size(); ++iProc) {
         for (plint blockId=ranges[iProc].first; blockId<=ranges[iProc].second; ++blockId) {
             threadAttribution -> addBlock(blockId, iProc);
+            printf("Block %lu is run by processor %lu\n", blockId, iProc);
         }
     }
 
