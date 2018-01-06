3b8b536028227afe24eb4aa503504d5a9836838f
Dwii <adrien.python@gmail.com>
2018-01-06 06:41:00 +0100

    Fix sbatch scripts

---
+++ implementation/Palabos/cavity_benchmark/launch_mpi.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+# Script to run with sbatch to execute benchmarks on slurm (for parallel execution)
+# sbatch -n <nb threads> launch_mpi.sh
+
+#SBATCH --partition=shared-gpu
+#SBATCH --time=04:00:00
+#SBATCH --gres=gpu:pascal:1
+
+module load CUDA
+module load GCC/4.9.3-2.25 OpenMPI/1.10.2
+#module load icc/2017.1.132-GCC-6.3.0-2.27 impi/2017.1.132 Python/3.6.1
+
+
+MPS=3
+ITER=100
+CHECK_NRG=true
+RR=1
+RS=1
+RC=1
+RPC=1
+RPD=1
+
+MPI=$(test $SLURM_TASKS_PER_NODE -eq 1 && echo true || echo false)
+ARGS="ITER=$ITER MPS=$MPS CHECK_NRG=$CHECK_NRG MPIparallel=$MPI RR=$RR RS=$RS RC=$RC RPC=$RPC RPD=$RPD MPICMD=srun"
+
+make benchmarks XPU=cpu BOUNDARY=false $ARGS
+make benchmarks XPU=cpu BOUNDARY=true  $ARGS
+make benchmarks XPU=gpu BOUNDARY=false $ARGS
+make benchmarks XPU=gpu BOUNDARY=true  $ARGS
