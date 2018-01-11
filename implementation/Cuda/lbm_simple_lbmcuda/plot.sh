5b577ff2cedbf7d53c9e6236a9ce3470edc2f571
Dwii <adrien.python@gmail.com>
2018-01-11 18:02:33 +0100

    Add directories creation

--- implementation/Cuda/lbm_simple_lbmcuda/plot.sh
+++ implementation/Cuda/lbm_simple_lbmcuda/plot.sh
@@ -6,11 +6,16 @@ out=out
 ben=$(basename $3 .ben)
 r=10
 
+curve_dir=time/curve
+repar_dir=time/repartion
+
+mkdir -p $repar_dir $curve_dir
+
 for gpu in $1;
 do
 	for bs in $2;
 	do
-		python3 plot_benchmark_times.py       $3 $gpu-$ben-$bs.pdf "$ben.ben Benchmark on $gpu GPU with $bs block size"       100 $r $out/lbm_${gpu}_B${bs}_$ben.dat "Temps restant" $out/lbm_${gpu}_B${bs}__repeat-s$r$ben.dat "Send" $out/lbm_${gpu}_B${bs}__repeat-r$r$ben.dat "Receive" $out/lbm_${gpu}_B${bs}__repeat-c$r$ben.dat "Collide & Stream" 
-		python3 plot_benchmark_times_curve.py $3 $gpu-$ben-$bs.pdf "Temps d'execution sur un GPU $gpu (blocs de $bs threads)" 100 $r $out/lbm_${gpu}_B${bs}_$ben.dat "Temps total"   $out/lbm_${gpu}_B${bs}__repeat-s$r$ben.dat "Send" $out/lbm_${gpu}_B${bs}__repeat-r$r$ben.dat "Receive" $out/lbm_${gpu}_B${bs}__repeat-c$r$ben.dat "Collide & Stream" 
+		python3 plot_benchmark_times.py       $3 $repar_dir/$gpu-$ben-$bs.pdf "$ben.ben Benchmark on $gpu GPU with $bs block size"       100 $r $out/lbm_${gpu}_B${bs}_$ben.dat "Temps restant" $out/lbm_${gpu}_B${bs}__repeat-s$r$ben.dat "Send" $out/lbm_${gpu}_B${bs}__repeat-r$r$ben.dat "Receive" $out/lbm_${gpu}_B${bs}__repeat-c$r$ben.dat "Collide & Stream" 
+		python3 plot_benchmark_times_curve.py $3 $curve_dir/$gpu-$ben-$bs.pdf "Temps d'execution sur un GPU $gpu (blocs de $bs threads)" 100 $r $out/lbm_${gpu}_B${bs}_$ben.dat "Temps total"   $out/lbm_${gpu}_B${bs}__repeat-s$r$ben.dat "Send" $out/lbm_${gpu}_B${bs}__repeat-r$r$ben.dat "Receive" $out/lbm_${gpu}_B${bs}__repeat-c$r$ben.dat "Collide & Stream" 
 	done
 done
